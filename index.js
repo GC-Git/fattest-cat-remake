52a8e55b50de27c1fa72484ac14be71099da7518
Gordon Clark <gordon2108@gmail.com>
2019-04-22 11:49:11 -0700

    Initial

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const path = require('path')
+
+const port = process.env.PORT || 9000
+
+const app = express()
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+// Put all API endpoints under '/api'
+app.get('/api/hello', (req, res) => {
+    res.send('Hello world!')
+    console.log(`Hello world!`);
+});
+  
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('/', (req, res) => {
+    console.log('Serving react app.')
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+  
+app.listen(port);
