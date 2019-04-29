4f385d0a506a1ba23e36e8747e42eff98da1f5d7
Gordon Clark <gordon2108@gmail.com>
2019-04-29 13:13:32 -0700

    Added route to get cats of a specified day

--- index.js
+++ index.js
@@ -1,36 +1,46 @@
+// Load our environmental variables from .env
 require('dotenv').config()
+const port = process.env.PORT || 9000
 
+// Helper libraries
 const express = require('express')
 const bodyParser = require('body-parser')
 const path = require('path')
-const mongoose = require('mongoose')
-
-// Schemas
-const CatSchema = require('./schemas/Cat')
-
-let Cat = mongoose.model('Cat', CatSchema)
-let FatCat = mongoose.model('FatCat', CatSchema)
-
-const port = process.env.PORT || 9000
+const dayjs = require('dayjs')
 
 // Database Connection
+const mongoose = require('mongoose')
 const database = process.env.MONGODB_URI || 'mongodb://localhost/fatcats'
+
+// Schemas for Mongoose
+const CatSchema = require('./schemas/Cat')
+
+// Mongoose models created from Schemas
+let Cat = mongoose.model('Cat', CatSchema)
+let FatCat = mongoose.model('FatCat', CatSchema)
 
 const app = express()
 
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'client/build')));
 
-// Put all API endpoints under '/api'
+
+
+
+// ===========================
+//           ROUTES
+// ===========================
+
+// NOTE: Put all API endpoints under '/api'
 app.get('/api/hello', (req, res) => {
     res.send('Hello world!')
     console.log(`Hello world!`);
 });
   
-// TODO: I KNOW this ought to go somewhere else. Figure that out.
+
+
 app.get('/api/fatcat', (req, res)=>{
     try{
-
         // TODO: Figure out if we should be creating a new connection every time someone calls the route.
         mongoose.connect(database, {useNewUrlParser: true});
         let db = mongoose.connection
@@ -47,6 +57,49 @@ app.get('/api/fatcat', (req, res)=>{
 })
 
 
+
+// Gets all the cats for a particular date
+app.get('/api/cats/:date', (req, res) => {
+    if(!dayjs(req.params.date).isValid()){
+        res.send('Invalid date')
+        return;
+    }
+
+    let requestedDate = dayjs(req.params.date).valueOf()
+    const startOfDay = dayjs(requestedDate).startOf('day').valueOf()
+    const endOfDay = dayjs(requestedDate).endOf('day').valueOf()
+
+    mongoose.connect(database, {useNewUrlParser: true});
+    let db = mongoose.connection
+    
+    db.on('error', console.error.bind(console, 'connection error:'))
+    db.once('open', function(){    
+        
+        Cat.find({
+            date: {
+                $lt: endOfDay,
+                $gte: startOfDay
+            }
+        })
+            .limit(50)
+            .exec((err, cats) => {
+                res.send(cats)
+                db.close()
+            })
+        
+    })
+
+})
+
+
+
+// Reroutes to todays date, getting all the cats for today
+app.get('/api/cats', (req, res)=>{
+    res.redirect('/api/cats/'+dayjs().format('MM-DD-YYYY'));
+})
+
+
+
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get('/', (req, res) => {
@@ -54,4 +107,6 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
   
+
+
 app.listen(port);
