03fe9e3f0bd4f0c46801931b6a2af2989a6d9ede
Gordon Clark <gordon2108@gmail.com>
2019-05-16 15:55:03 -0700

    Added scroller to Hero component.

--- client/src/components/Hero/Hero.js
+++ client/src/components/Hero/Hero.js
@@ -8,6 +8,7 @@ export default function Hero(props){
             <img className="hero__cat-logo" src={require('./CatLogo.png')} alt="A beautiful fat cat." />
             <h2 className="hero__header">Locally sourced obese cats</h2>
             <p className="hero__sub-header">Ethical entertainment for a modern era.</p>
+            <img className="hero__scroller" src={require('./Scroll@2x.png')} alt="Indicates the page can be scrolled." />
         </div>
     )
 }
