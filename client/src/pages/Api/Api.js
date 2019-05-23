7b3fd2e1944386c7fb4d3ccd93c481af21431c40
GC-Git <gordondelfel@gmail.com>
2019-05-23 14:41:01 -0700

    Fixed API links in api page.

--- client/src/pages/Api/Api.js
+++ client/src/pages/Api/Api.js
@@ -4,6 +4,16 @@ import './Api.css'
 
 
 export default class Api extends React.Component {
+    constructor(props){
+        super(props)
+
+        let today = new Date();
+        let oneWeekAgo = new Date();
+        oneWeekAgo.setDate(oneWeekAgo.getDate() - 7)
+
+        this.state = { today, oneWeekAgo }
+    }
+
     render() {
         return (
             <React.Fragment>
@@ -21,6 +31,24 @@ export default class Api extends React.Component {
                         <ul className="apiPage__list">
                             <li className="apiPage__endpoint_list_item">
                                 <a className="apiPage__endpoint_link" href="/api/fatcat">/api/fatcat</a>
+                            </li>
+                            <li className="apiPage__endpoint_list_item">
+                                <a className="apiPage__endpoint_link" href={"/api/fatcat/" + this.state.today}>/api/fatcat/:date</a>
+                            </li>
+                            <li className="apiPage__endpoint_list_item">
+                                <a className="apiPage__endpoint_link" href={"/api/fatcat/range/" + this.state.oneWeekAgo + "/" + this.state.today}>/api/fatcat/range/:dateStart/:dateEnd</a>                                
+                            </li>
+                            <li className="apiPage__endpoint_list_item">
+                                <a className="apiPage__endpoint_link" href="/api/cats">/api/cats</a>     
+                            </li>
+                            <li className="apiPage__endpoint_list_item">
+                                <a className="apiPage__endpoint_link" href="/api/cats/week">/api/cats/week</a>               
+                            </li>
+                            <li className="apiPage__endpoint_list_item">
+                                <a className="apiPage__endpoint_link" href={"/api/cats/day/" + this.state.today}>/api/cats/day/:date</a>
+                            </li>
+                            <li className="apiPage__endpoint_list_item">
+                                <a className="apiPage__endpoint_link" href={"/api/cats/range/" + this.state.oneWeekAgo + "/" + this.state.today}>/api/cats/range/:dateStart/:dateEnd</a>
                             </li>
                         </ul>
                     </div>
