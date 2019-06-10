370dd5a58f558824a74fb91e6cb8c1ae1b63d315
GC-Git <gordondelfel@gmail.com>
2019-06-10 14:04:11 -0700

    Finished API.

--- client/src/pages/Api/Api.js
+++ client/src/pages/Api/Api.js
@@ -30,24 +30,51 @@ export default class Api extends React.Component {
                         </h2>
                         <ul className="apiPage__list">
                             <li className="apiPage__endpoint_list_item">
+                                <p className="apiPage__endpoint_label">
+                                    Cat by ID:
+                                </p>
+                                <a className="apiPage__endpoint_link" href="/api/cats/week">/api/catId/148184</a>               
+                            </li>
+
+                            <li className="apiPage__endpoint_list_item">
+                                <p className="apiPage__endpoint_label">
+                                    Todays fat cat:
+                                </p>
                                 <a className="apiPage__endpoint_link" href="/api/fatcat">/api/fatcat</a>
                             </li>
+
                             <li className="apiPage__endpoint_list_item">
+                                <p className="apiPage__endpoint_label">
+                                    Fattest cat of given date:
+                                </p>
                                 <a className="apiPage__endpoint_link" href={"/api/fatcat/" + this.state.today}>/api/fatcat/:date</a>
                             </li>
+
                             <li className="apiPage__endpoint_list_item">
+                                <p className="apiPage__endpoint_label">
+                                    Fattest cat of a date range:
+                                </p>
                                 <a className="apiPage__endpoint_link" href={"/api/fatcat/range/" + this.state.oneWeekAgo + "/" + this.state.today}>/api/fatcat/range/:dateStart/:dateEnd</a>                                
                             </li>
+
                             <li className="apiPage__endpoint_list_item">
+                                <p className="apiPage__endpoint_label">
+                                    All cats today:
+                                </p>
                                 <a className="apiPage__endpoint_link" href="/api/cats">/api/cats</a>     
                             </li>
+
                             <li className="apiPage__endpoint_list_item">
-                                <a className="apiPage__endpoint_link" href="/api/cats/week">/api/cats/week</a>               
-                            </li>
-                            <li className="apiPage__endpoint_list_item">
+                                <p className="apiPage__endpoint_label">
+                                    All cats of a given day:
+                                </p>
                                 <a className="apiPage__endpoint_link" href={"/api/cats/day/" + this.state.today}>/api/cats/day/:date</a>
                             </li>
+
                             <li className="apiPage__endpoint_list_item">
+                                <p className="apiPage__endpoint_label">
+                                    All cats of a date range:
+                                </p>
                                 <a className="apiPage__endpoint_link" href={"/api/cats/range/" + this.state.oneWeekAgo + "/" + this.state.today}>/api/cats/range/:dateStart/:dateEnd</a>
                             </li>
                         </ul>
