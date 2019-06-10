8fa65db5f17ce5555e0767641e383f34ec5b3e51
GC-Git <gordondelfel@gmail.com>
2019-06-10 14:59:18 -0700

    Formatting changes to about page.

--- client/src/pages/About/About.js
+++ client/src/pages/About/About.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Hero, BGWithCurve, InfoCardCollection, Testimonial } from "../../components"
-// import './About.css'
+import './About.css'
 
 export default class About extends React.Component {
     render(){
@@ -15,13 +15,8 @@ export default class About extends React.Component {
 
                 <BGWithCurve>
                 <InfoCardCollection />
-                <p style={{
-                    fontSize: "28px",
-                    alignText: "center",
-                    margin: "1.5em 2em 2.5em 2em",
-                    textAlign: 'justify'
-                }}>
-                    All the cats shown here are from the Oregon Humane Society and can be adopted via their website. Each morning, we get the freshest, fattest cats at OHS and display them here for your pleasure.
+                <p className="infocardcollection__text">
+                    All the cats shown on this site are courtesy of the Oregon Humane Society. Each morning, we get the fattest cat at OHS and display it here for your pleasure. The fattest cat of the day is displayed along side a chart showing the history of the cats weight. One of these ham beasts catch your eye? Click 'Details' under the cats image to go to its page at the Oregon Humane Society, where you can begin the adoption process!
                 </p>
                 </BGWithCurve>
                 <Testimonial 
